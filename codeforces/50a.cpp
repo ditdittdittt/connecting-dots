9df1456b5c0da7b052f28a2956fe9f5194831c2c
ditdittdittt <ditdittdittt@gmail.com>
2023-07-16 10:36:00 +0700

    50a

---
+++ codeforces/50a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// https://codeforces.com/problemset/problem/50/A
+void solve() {
+    int m,n;
+
+    cin >> m >> n;
+
+    int output = (m * n) / 2;
+    cout << output << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
